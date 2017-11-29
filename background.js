985b9ba7d7a4f44ad073b7ead17affa0be8f3de6
Varun Chembukkavu <varun.chembukkavu@stonybrook.edu>
2017-11-29 02:31:58 -0500

    Created a basic extension that can compare screenshots and highlight the changes in red

---
+++ background.js
@@ -0,0 +1,50 @@
+//Dictionary object to store screenshots
+var dict = new Object();
+
+//Take a screenshot of the current active tab every 30 seconds and replace the previous screenshot for the given tab.
+(function(){
+    chrome.tabs.captureVisibleTab(function(screenshotUrl) {
+        chrome.tabs.query({
+            active:true,
+            windowType:"normal",
+            currentWindow: true},
+            function(d){
+                if(d[0]!=undefined){
+                    dict[d[0].id]=screenshotUrl;
+                }
+            }
+        )
+    })
+    setTimeout(arguments.callee, 10000);
+})();
+
+//Whenver a change of tab is detected compare the new tab with the previous tab and highlight the change in red using rembrandt
+chrome.tabs.onActivated.addListener( function(newTab) {
+    var tabId = newTab.tabId;
+    if(dict[tabId]){
+        chrome.tabs.captureVisibleTab(function(screenshotUrl) {
+            const rembrandt = new Rembrandt({
+                imageA: dict[tabId],
+                imageB: screenshotUrl,
+                thresholdType: Rembrandt.THRESHOLD_PERCENT,
+                maxThreshold: 1,
+                maxDelta: 20,
+                maxOffset: 0,
+                renderComposition: true, // Should Rembrandt render a composition image?
+                compositionMaskColor: Rembrandt.Color.RED // Color of unmatched pixels
+            })
+
+            rembrandt.compare()
+            .then(function (result) {
+                console.log('Passed:', result.passed)
+                console.log('Pixel Difference:', result.differences, 'Percentage Difference', result.percentageDifference, '%')
+                console.log('Composition image buffer:', result.compositionImage)
+
+            })
+            .catch((e) => {
+                console.error(e)
+            })
+        })
+    }
+
+});

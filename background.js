1e5c7b6350efea5dd540bb96dcb66ceff2047f0f
Varun Chembukkavu <varun.chembukkavu@stonybrook.edu>
2017-11-29 03:54:00 -0500

    Added a sleep timer before capturing screenshot on new tab

--- background.js
+++ background.js
@@ -21,30 +21,39 @@ var dict = new Object();
 //Whenver a change of tab is detected compare the new tab with the previous tab and highlight the change in red using rembrandt
 chrome.tabs.onActivated.addListener( function(newTab) {
     var tabId = newTab.tabId;
-    if(dict[tabId]){
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    async function wait() {
+        await sleep(2000);
         chrome.tabs.captureVisibleTab(function(screenshotUrl) {
-            const rembrandt = new Rembrandt({
-                imageA: dict[tabId],
-                imageB: screenshotUrl,
-                thresholdType: Rembrandt.THRESHOLD_PERCENT,
-                maxThreshold: 1,
-                maxDelta: 20,
-                maxOffset: 0,
-                renderComposition: true, // Should Rembrandt render a composition image?
-                compositionMaskColor: Rembrandt.Color.RED // Color of unmatched pixels
-            })
+            if(dict[tabId]){
+                const rembrandt = new Rembrandt({
+                    imageA: screenshotUrl,
+                    imageB: dict[tabId],
+                    thresholdType: Rembrandt.THRESHOLD_PERCENT,
+                    maxThreshold: 1,
+                    maxDelta: 20,
+                    maxOffset: 0,
+                    renderComposition: true, // Should Rembrandt render a composition image?
+                    compositionMaskColor: Rembrandt.Color.RED // Color of unmatched pixels
+                })
 
-            rembrandt.compare()
-            .then(function (result) {
-                console.log('Passed:', result.passed)
-                console.log('Pixel Difference:', result.differences, 'Percentage Difference', result.percentageDifference, '%')
-                console.log('Composition image buffer:', result.compositionImage)
+                rembrandt.compare()
+                .then(function (result) {
+                    console.log('Passed:', result.passed)
+                    console.log('Pixel Difference:', result.differences, 'Percentage Difference', result.percentageDifference, '%')
+                    console.log('Composition image buffer:', result.compositionImage)
 
-            })
-            .catch((e) => {
-                console.error(e)
-            })
+                })
+                .catch((e) => {
+                    console.error(e)
+                })
+            }else{
+                dict[tabId]=screenshotUrl;
+            }
         })
     }
+    wait();
 
 });
